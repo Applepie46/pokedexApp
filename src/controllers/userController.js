1149803b0305fcc3b61796a10c3717643de2840b
Applepie46 <emmanuel.cadavid46@gmail.com>
2022-12-29 19:20:52 -0500

    [Emmanuel][v-0.2] Pokemon detail endpoint done and images on pokedex fixed

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,22 @@
 const userController = {
-    getAllPokemons: async (req, res) => {
+    getAllPokemons: (req, res) => {
         try {
-            await fetch("https://pokeapi.co/api/v2/pokemon?limit=1154&offset=0")
+            fetch("https://pokeapi.co/api/v2/pokemon?limit=1154&offset=0")
                 .then(response => response.json())
                 .then(data => {
                     res.render("pokemonList", { data })
+                })
+        } catch (error) {
+            res.render("error");
+            console.log(error);
+        }
+    }, 
+    pokemonDetail: (req, res) => {
+        try {
+            fetch(`https://pokeapi.co/api/v2/pokemon/${req.params.name}`)
+                .then(response => response.json())
+                .then(data => {
+                    res.render("pokemonDetail", { data });
                 })
         } catch (error) {
             res.render("error");
--- src/routes/users.js
+++ src/routes/users.js
@@ -4,5 +4,6 @@ const userController = require("../controllers/userController");
 
 /* GET users listing. */
 router.get('/pokedex', userController.getAllPokemons);
+router.get("/pokemonDetail/:name", userController.pokemonDetail);
 
 module.exports = router;
