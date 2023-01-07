5c1c36aa9f5004f1ff36ea6a17a49ec6315998df
Applepie46 <emmanuel.cadavid46@gmail.com>
2023-01-06 19:07:15 -0500

    [Emmanuel][v-0.2][feat] Correcting form submit and page icon

--- public/javascripts/searchBar.js
+++ public/javascripts/searchBar.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const searchForm = document.querySelector("")
+    const searchForm = document.querySelector(".search-form");
+    const searchInput = document.getElementById("seach-bar");
+
+    searchForm.addEventListener("submit", (e) => {
+        let errors = [];
+        e.preventDefault();
+        if (searchInput.value === "") {
+            errors.push("You must type something on the field")
+        }
+
+        if (errors.length === 0) {
+            searchForm.submit()
+        }
+
+    })
 });
